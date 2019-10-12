97f2006e98f617171f972dce1fb91fcbbf270d8a
Leduar Araujo <LeduarAraujo>
2019-10-12 00:40:14 -0300

    Ajuste do local de armazenamento da logo.

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+declare var require: any;
+
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
+
+  
 
   constructor() { }
 
