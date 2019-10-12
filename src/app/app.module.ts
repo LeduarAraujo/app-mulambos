bdb03954dd4ddd8a58b0272376b3560f458ee938
Leduar Araujo <LeduarAraujo>
2019-10-11 23:57:32 -0300

    Criação do projeto do site mulambos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuRodapeComponent } from './components/menu-rodape/menu-rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuRodapeComponent
   ],
   imports: [
     BrowserModule,
