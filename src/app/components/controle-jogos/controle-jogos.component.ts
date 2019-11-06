1ed5ba580d7e052fd9f01a8db1a32ddf7b5a5f7c
Leduar Araujo <LeduarAraujo>
2019-11-06 00:54:18 -0300

    Implementação da parte dinamica do html em dinamizar a tabela

--- src/app/components/controle-jogos/controle-jogos.component.ts
+++ src/app/components/controle-jogos/controle-jogos.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controle-jogos.component.css']
 })
 export class ControleJogosComponent implements OnInit {
-
+  listOpr: any;
   constructor() { }
 
   ngOnInit() {
+    this.listOpr = [
+      {
+        "nmOpr":"Leduar",
+        "aplOpr":"Marshalls",
+        "clOpr":"DMR",
+        "ntFirstGun":"OK",
+        "fpsFirstGun":"450",
+        "ntSecondGun":"OK",
+        "fpsSecondGun":"348",
+        "siOpr":true
+      }
+    ];
   }
+
+
+
+
+
+  
 
 }
